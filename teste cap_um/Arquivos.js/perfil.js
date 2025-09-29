861a49a6a42a90973f6fa9d5f8366049ca5b6c3a
Esther Barbosa <estherbguimaraes@gmail.com>
2025-09-29 13:57:39 -0300

    Arquivos js e css

---
+++ teste cap_um/Arquivos.js/perfil.js
@@ -0,0 +1,15 @@
+const form = document.getElementById ("form-cadastro");
+const nome = document.getElementById ("nome");
+const email = document.getElementById ("email");
+const data = document.getElementById ("data_nascimento");
+const cpf = document.getElementById ("CPF");
+const telefone = document.getElementById ("telefone");
+const senha = document.getElementById ("senha");
+
+
+form.addEventListener("submit",(Event) => {
+    Event.preventDefault();
+    if (nome.value===""){
+        
+    }
+})
