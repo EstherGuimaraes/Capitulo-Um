8665892efeca2703e87a25b6d159d37a2c1d5ffa
Esther Barbosa <estherbguimaraes@gmail.com>
2025-09-29 08:21:51 -0300

    data-favorito

--- teste cap_um/Arquivos.js/Cap_um.js
+++ teste cap_um/Arquivos.js/Cap_um.js
@@ -18,16 +18,24 @@ const todosOsBotoes = document.querySelectorAll('.livrofv-btn');
 // 2. Itera sobre cada botão encontrado e adiciona um "escutador" de clique
 todosOsBotoes.forEach(botao => {
     botao.addEventListener('click', () => {
+        // botao.classList.toggle('botaofv');
+        const estaFavoritado = botao.dataset.favoritado === "true";
         // 3. Dentro do botão que foi CLICADO, encontra a imagem (img)
         const imagem = botao.querySelector('img');
 
         // 4. Altera o 'src' apenas dessa imagem específica
-        if(imagem.src.includes('/IMG/favorito vermelho.png')){
+        if (estaFavoritado) {
+            // Se está favoritado, altera o atributo e a imagem para o estado 'não favoritado'
+            botao.dataset.favoritado = "false";
             imagem.src = '/IMG/favoritosPreto.png';
             imagem.alt = 'Item não favoritado';
-        }else{
+            console.log("Desfavoritado: mudei para o estado false");
+        } else {
+            // Se não está favoritado, altera o atributo e a imagem para o estado 'favoritado'
+            botao.dataset.favoritado = "true";
             imagem.src = '/IMG/favorito vermelho.png';
             imagem.alt = 'Item favoritado';
+            console.log("Favoritado: mudei para o estado true");
         }
         
     });
