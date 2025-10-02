75729da527655b7c96dbc09695b9f095c877e8c6
AmandaLourenco11 <amandalourenco.2512@gmail.com>
2025-10-02 12:21:50 -0300

    atualizações
    carrinho

--- teste cap_um/Arquivos.js/carrinho.js
+++ teste cap_um/Arquivos.js/carrinho.js
@@ -1,73 +1,13 @@
-function adicionarCarrinho(nome, preco) {
+function adicionarCarrinho () {
 
 
-    const item = { nome, preco };
+    const Item = document.createElement('li');
+    Item.classList.add('itemCarrinho');
+    adicionarCarrinho.push(Item);
 
 
-    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-
-
-    carrinho.push(item);
-
-
-    localStorage.setItem('carrinho', JSON.stringify(carrinho));
-
-
-    alert(`${nome} adicionado ao carrinho!`);
-};
-
-
-
-
-function mostrarCarrinho() {
-   
-
-
-
-
-let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
-let lista = document.getElementById('lista-carrinho');
-let total = 0;
-
-
-if (lista) {
-    lista.innerHTML = "";
-
-
-carrinho.forEach(item => {
-    let li = document.createElement('li');
-    li.textContent = `${item.nome} - R$ ${item.preco.toFixed(2)}`;
-    lista.appendChild(li);
-    total += item.preco;
-    });
-
-
-    document.getElementById('total').textContent = `Total: R$${total.toFixed(2)}`
-    }
 }
 
-
-function limparCarrinho() {
-    localStorage.removeItem('carrinho');
-    mostrarCarrinho();
-}
-
-
-document.addEventListener('DOMContentLoaded', mostrarCarrinho);
-
-
-document.getElementById('finalizarCompra')?.addEventListener('click', function() {
-    localStorage.setItem("totalCommpra", total);
-    window.location.href = "finalizacao.html";
-});
-
-
-
-
-
-
-
-
-
-
-
+const iconeRemover = document.createElement('button');
+    iconeRemover.classList.add('removerCarrinho');
+    iconeRemover.setAttribute
